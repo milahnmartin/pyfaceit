4c17af9ca616009f30f373f9a22c403a46f55386
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-01-06 16:03:02 +0200

    Changed setup file to it corresponds with my pip package

--- setup.py
+++ setup.py
@@ -7,22 +7,25 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = "0.0.1"
+DESCRIPTION = "Simple Package that allows the extraction of player data, this is a REST api and needs to be ran. This is a flask application"
+LONG_DESCRIPTION = """Popular Online Gaming Platform allows games like counterstrike and much more to be played on their servers,
+                Faceit has created a pretty inrteresting and hard to understand API that can extract player and game data, this package will do all the hard backend stuf for you so you can just simply import the package and query
+                a player, game or map.
+                """
 
 # Setting up
 setup(
-    name="vidstream",
+    name="pyfaceit",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Milahn Martin",
+    author_email="<milahnmartin.develop@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=["requests", "flask", "bs4"],
+    keywords=["python", "faceit", "api", "rest api", "get data", "flask"],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
@@ -30,5 +33,5 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
 )
