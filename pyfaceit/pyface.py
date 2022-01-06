e80cdc37d98b7f2da2073eb1eef3f3b74379f7ad
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2022-01-06 20:39:39 +0200

    added main faceit class with data
    added main faceit class and also the sys module that helps to get files from other folder

--- pyfaceit/pyface.py
+++ pyfaceit/pyface.py
@@ -1,2 +1,12 @@
-def SayHi():
-    print("hello how are you")
+import requests
+import sys
+
+sys.path.insert(1,'/pyfaceit/games')
+import games
+
+if __name__ == "__main__":
+    instance = games.Pyfaceit('Ultrafy','de_mirage')
+    print(instance.player_stats_map())
+    
+
+
